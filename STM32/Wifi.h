61329174fe89d64b34e319234a8b9e99fb761794
jeff-901 <b07901052@ntu.edu.com>
2021-06-26 16:59:02 +0800

    update to remote server ip

--- STM32/Wifi.h
+++ STM32/Wifi.h
@@ -4,9 +4,9 @@
 #include "TCPSocket.h"
 #include "Sensor.h"
 #include <string>
-#define IP_ADDR "172.20.10.7"
+#define IP_ADDR "34.80.242.27"
 // "192.168.43.180" "172.20.10.7" "34.80.242.27"
-#define PORT_NUM 8001
+#define PORT_NUM 4000
 const char *sec2str(nsapi_security_t sec) {
     switch (sec) {
     case NSAPI_SECURITY_NONE:
